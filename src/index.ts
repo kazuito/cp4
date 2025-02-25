af544ea58b49625c236896ee95d52a573509ea98
Kazuma Ito <zarigani2x@gmail.com>
2025-02-26 03:36:36 +0900

    watch to copy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hi, I'm cp4!");
+import { program } from "commander";
+import { cp } from "fs/promises";
+import { CopyOptions, watch, WatchOptions } from "fs";
+
+async function main() {
+  program
+    .option("-r, --recursive", "copy directories recursively")
+    .argument("<file>", "file/dir to copy")
+    .argument("<dest>", "destination");
+
+  program.parse();
+
+  const options = program.opts();
+  const [filePath, destPath] = program.args;
+
+  const copyOptions: CopyOptions = {
+    recursive: !!options.recursive,
+  };
+  const watchOptions: WatchOptions = {
+    recursive: !!options.recursive,
+  };
+
+  const copy = async () => {
+    await cp(filePath, destPath, copyOptions);
+  };
+
+  // watch for changes
+  watch(filePath, watchOptions, async (eventType, filename) => {
+    await copy();
+    console.log(`[watch] copied "${filePath}/${filename}" to "${destPath}/${filename}"`);
+  });
+
+  // initial copy
+  await copy();
+  console.log(`[watch] copied "${filePath}" to "${destPath}"`);
+}
+
+try {
+  main();
+} catch (error) {
+  console.error(error);
+}
